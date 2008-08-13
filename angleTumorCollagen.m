0e71a1c3a89c4320cf1f22b00ff3b652251aa67e
Jared Doot <doot@wisc.edu>
2008-08-13 15:51:16 +0000

    Allowed for user to input a distance threshold, also am plotting the center points of the curvelets

--- angleTumorCollagen.m
+++ angleTumorCollagen.m
@@ -2,7 +2,6 @@
 
 function angleTumorCollagen(img)
 
-
 hull = get(gcf,'userdata');
 
 %smooth out the hull data
@@ -32,10 +31,46 @@ angles = collAngle(C_indent);
 
 centers = groupCenter(center_pos);
 
+% make the centers into plottable vectors
+
+[x,y] = centerPlot(centers);
+
+% disreguard the curvelets "too" far away from the center
+
+hh = findobj(gcf,'Tag','dist');
+
+dist_micron = str2num(get(hh,'String'));
+
+% checks to see if dist_max is empty
+
+if isempty(dist_micron)
+    dist_flag = 1;
+else
+    dist_flag = 0;
+end
+
+% convert distance into pixel amount
+
+img_sz = size(img);
+
+wid = img_sz(2);
+
+HH = findobj(gcf,'Tag','micron');
+
+microns = str2num(get(HH,'String'));
+
+dist_max = wid/microns * dist_micron;
+
+centers_x = dropFar(centers,hull,dist_max);
+
+[xx,yy] = centerPlot(centers_x);
+
 % calculate angles
 angle_tumor = cell(32,1);
 
 h = waitbar(0,'Computing');
+if dist_flag == 1;
+    figure(1);plot(y,x,'yd');
 for ii = 1:length(centers)
     goto = size(centers{ii});
     if goto(2) ==0;
@@ -50,6 +85,23 @@ for ii = 1:length(centers)
     end
     waitbar(ii/length(centers),h);
 end
+else
+    figure(1);plot(yy,xx,'yd');
+for ii = 1:length(centers_x)
+    goto = size(centers_x{ii});
+    if goto(2) ==0;
+        continue
+    else
+        for jj = 1:goto(2);
+        center = [centers_x{ii}(1,jj),centers_x{ii}(2,jj)];
+        angle = angles{ii};
+       % hull = [100 100;100 399;399 399;399 100]; % this is user defined
+        angle_tumor{ii}(jj) = angleLine(center, angle, hull);
+        end
+    end
+    waitbar(ii/length(centers_x),h);
+end
+end
 close(h)
 angle_vec = 0;
 % make into vector
@@ -61,6 +113,12 @@ end
 
 angle_vec = angle_vec(2:end);
 figure;
-hist(angle_vec,9)
+hist(angle_vec,9);
+
+mean_angle = mean(angle_vec)
+std_angle = std(angle_vec)
+
+mean_angle_no_zero = mean(angle_vec(find(angle_vec~=0)))
+std_angle_no_zero = std(angle_vec(find(angle_vec~=0)))
 
 
