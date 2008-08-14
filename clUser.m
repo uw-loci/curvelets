6f40ae98e766800ffe4ffca6ca9fdab721628097
Jared Doot <doot@wisc.edu>
2008-08-14 14:31:01 +0000

    changed clUser to completly redraw window. also just fixed dropFar to be a function

--- clUser.m
+++ clUser.m
@@ -3,6 +3,15 @@ function clUser(arg)
 global img;
 
 if nargin == 0;
-clear figure;
+close all;
 end
-image(img/2);colormap(gray);axis('image');
+imagefig=figure('units','normalized','position',[0.1 0.1 0.8 0.8]);    
+
+figure(1);image(img/2);colormap(gray);axis('image');
+
+hold on
+
+set(imagefig,'windowbuttondownfcn',{@track}); 
+butGo('arg',img);
+butClear;
+fillDist('arg',img);
