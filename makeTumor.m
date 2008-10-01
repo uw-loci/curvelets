5d68a79f7aeefdf60fa08709eb341b8853d4196f
Jared Doot <doot@wisc.edu>
2008-10-01 00:18:22 +0000

    Starting over

--- makeTumor.m
+++
@@ -1,8 +0,0 @@
-function [mat] = makeTumor(group_angle)
-
-mat = zeros(512);
-x = max(max(group_angle))*ones(300,1);
-mat(100:399,100) = x;
-mat(100,100:399) = x';
-mat(100:399,399) = x;
-mat(399,100:399) = x';
