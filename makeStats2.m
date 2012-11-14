b5b65f67b7e49fbdf9af7f2cc61e5c51662b0357
Jeremy Bredfeldt <jeremybredfeldt@gmail.com>
2012-11-14 15:31:18 -0600

    V2 release attempt
    I am trying to release the features of V2.0, which is everything I've
    been working on to date.

--- makeStats2.m
+++ makeStats2.m
@@ -1,23 +1,27 @@
 %--------------------------------------------------------------------------
 % function for calculating statistics
-    function stats = makeStats2(vals,tempFolder,imgName,map)
-         aveAngle = mean(vals);
-         medAngle = median(vals);
-         stdAngle = std(vals);         
-         refStd = 48.107;
-        
-         alignMent = 1-(stdAngle/refStd);
-         
-         %threshold the map file: 0-255 maps to 0 to 90 degrees
-         redThresh = 60*(255/90);
-         yelThresh = 45*(255/90);
-         grnThresh = 20*(255/90);
-         %count the number of pixels that are greater than the thresholds
-         redMap = sum(sum(map>redThresh));
-         yelMap = sum(sum(map>yelThresh));
-         grnMap = sum(sum(map>grnThresh));
-       
-         stats = vertcat(aveAngle,medAngle,stdAngle,alignMent,redMap,yelMap,grnMap);
-         saveStats = fullfile(tempFolder,strcat(imgName,'_stats.csv'));
-         csvwrite(saveStats,stats)
-    end
+function stats = makeStats2(vals,tempFolder,imgName,map,tr,ty,tg)
+
+%vals is a column vector of angles
+%temp Folder is the output folder
+%imgName is the name of the original image file
+%map is the 2D map image for counting pixels crossing thresholds
+%tr, ty, tg are red, yellow and green thresholds respectively
+
+    aveAngle = mean(vals);
+    medAngle = median(vals);
+    stdAngle = std(vals);         
+    refStd = 48.107;
+
+    alignMent = 1-(stdAngle/refStd);
+
+    %threshold the map file:
+    %count the number of pixels that are greater than the thresholds
+    redMap = sum(sum(map>tr));
+    yelMap = sum(sum(map>ty))-redMap;
+    grnMap = sum(sum(map>tg))-redMap-yelMap;
+
+    stats = vertcat(aveAngle,medAngle,stdAngle,alignMent,redMap,yelMap,grnMap);
+    saveStats = fullfile(tempFolder,strcat(imgName,'_stats.csv'));
+    csvwrite(saveStats,stats)
+end
