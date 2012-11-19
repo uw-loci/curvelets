19211578121fab86b7f8f4e89a6f43e7f115e54a
Jeremy Bredfeldt <jeremybredfeldt@gmail.com>
2012-11-19 07:45:00 -0600

    Improved efficiency
    Completely changed filter algorithms.

--- drawMap.m
+++ drawMap.m
@@ -16,50 +16,64 @@ function [rawmap, procmap] = drawMap(object, angles, img, bndryMeas)
         end
     end
     
-    if bndryMeas
-        %max filter the map
-        fSize = 8;
-        fSize2 = ceil(fSize/2);
-        map2 = nan(size(img));
-        for i = fSize2+1:size(img,1)-(fSize2+1)
-            for j = fSize2+1:size(img,2)-(fSize2+1)
-                map2(i,j) = nanmax(nanmax(rawmap(i-fSize2:i+fSize2,j-fSize2:j+fSize2)));
-            end
-        end        
-        
-        %Gaussian blur the map
-        sig = 4; %in pixels
-        h = fspecial('gaussian', [10*sig 10*sig], sig);
-        procmap = imfilter(uint8(map2),h,'replicate');
-    else
-        %Stnd Dev Filter the Map
-        fSize = 8;
-        fSize2 = ceil(fSize/2);
-        map2 = nan(size(img));
-        for i = fSize2+1:size(img,1)-(fSize2+1)
-            for j = fSize2+1:size(img,2)-(fSize2+1)
-                submap = rawmap(i-fSize2:i+fSize2,j-fSize2:j+fSize2);
-                ind = find(~isnan(submap));
-                if (length(ind)>2)
-                    map2(i,j) = nanstd(submap(ind));
-                end
-            end
-        end
-        
-        %invert to make this a measure of alignment
-        for i = 1:size(img,1)
-            for j = 1:size(img,2)
-                if (map2(i,j) > 0.5)
-                    map2(i,j) = 1/map2(i,j);
-                end
-            end
-        end
-        map2 = map2*255/nanmax(nanmax(map2));
-        %procmap = map2;
-        %Gaussian blur the map
-        sig = 4; %in pixels
-        h = fspecial('gaussian', [10*sig 10*sig], sig);
-        procmap = imfilter(uint8(map2),h,'replicate');  
-    end
+    %find the positions of all non-nan values
+    map2 = rawmap;
+    [J I] = size(img);
+    ind = find(~isnan(rawmap));
+    [y x] = ind2sub([J I],ind);    
     
+	if ~bndryMeas                     
+        %standard deviation filter
+        fSize = 64;
+        fSize2 = ceil(fSize/2);                
+        map2 = nan(size(img));        
+        for i = 1:length(ind)
+            %find any positions that are in a square region around the
+            %current curvelet
+            ind2 = find(x > x(i)-fSize2 & x < x(i)+fSize2 & y > y(i)-fSize2 & y < y(i)+fSize2);
+            %convert these positions to linear indices
+            ind3 = sub2ind([J I],y(ind2),x(ind2));
+            %get all the grey level values
+            vals = rawmap(ind3);
+            if length(vals) > 2
+                %only compute standard deviation if there are more than 2
+                %values
+                map2(y(i),x(i)) = std(vals);
+            end
+        end    
+        %figure(600); imagesc(map2); colorbar;
+        
+        map2 = 127.5 - map2;
+        %figure(650); imagesc(map2); colorbar;
+    end
+    %max filter
+    fSize = 12;
+    fSize2 = ceil(fSize/2);
+    map4 = nan(size(img));
+    tic
+    for i = 1:length(ind)
+        val = map2(y(i),x(i));
+        rows = y(i)-fSize2:y(i)+fSize2;
+        cols = x(i)-fSize2:x(i)+fSize2;
+        %get rid of out of bounds coordinates
+        ind4 = find(rows > 0 & rows < J & cols > 0 & cols < I);
+        rows = rows(ind4);
+        cols = cols(ind4);
+        %now make a square collection of indices
+        lenInd = length(ind4);
+        lenInd2 = lenInd*lenInd;
+        rows = repmat(rows,1,lenInd);
+        cols = reshape(repmat(cols,lenInd,1),1,lenInd2);
+        %get the linear indices in the original map
+        ind5 = sub2ind([J I],rows,cols);
+        %set the value to the max of the current or what was there
+        map4(ind5) = max(map4(ind5),val);
+    end
+    %figure(675); imagesc(map4); colorbar;
+    %gaussian filter
+    sig = 4; %in pixels
+    h = fspecial('gaussian', [10*sig 10*sig], sig);
+    %uint 8 converts nans to zeros
+    procmap = imfilter(uint8(map4),h,'replicate');
+    %figure(700); imagesc(procmap); colorbar;
 end
