b9df4d01eb48030c3fb1ac40a62ad321de5593b7
Jeremy Bredfeldt <jeremybredfeldt@gmail.com>
2012-11-30 13:14:20 -0600

    Batch statistics output
    This function writes a bunch of stat data out during the batch process,
    summarizing all of the results into one file. The organization of this
    could be improved.

---
+++ writeAllHistData2.m
@@ -0,0 +1,21 @@
+%function writeAllHistData(hist,bins,name_list,topLevelDir)
+function writeAllHistData2(histData, NorT, outDir, checkFirst, stats, imgName, sliceNum)
+    if checkFirst == 1
+        %if it's the first file after starting the program, start a new output file
+        fid = fopen([outDir 'AllHistData.txt'],'w+');
+    else
+        %append the current file
+        fid = fopen([outDir 'AllHistData.txt'],'a+');
+    end
+            
+    for ii = 1:size(histData,1)
+        fprintf(fid,'%s\t%d\t%.2f\t%d\t%s\t',imgName,sliceNum,histData(ii,1),histData(ii,2),NorT);
+        if ii == 1
+            for jj = 1:size(stats)
+                fprintf(fid,'%0.4f\t',stats(jj));
+            end
+        end
+        fprintf(fid,'\r\n');
+    end    
+    fclose(fid);
+end
