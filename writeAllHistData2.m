932d69b273817d94eb1027e711b28f80e7e19a0b
Jeremy Bredfeldt <jeremybredfeldt@gmail.com>
2013-02-06 23:12:02 -0600

    Added code to handle case where no fibers are found in image

--- writeAllHistData2.m
+++ writeAllHistData2.m
@@ -7,15 +7,21 @@ function writeAllHistData2(histData, NorT, outDir, checkFirst, stats, imgName, s
         %append the current file
         fid = fopen([outDir 'AllHistData.txt'],'a+');
     end
-            
-    for ii = 1:size(histData,1)
-        fprintf(fid,'%s\t%d\t%.2f\t%d\t%s\t',imgName,sliceNum,histData(ii,1),histData(ii,2),NorT);
-        if ii == 1
-            for jj = 1:size(stats)
-                fprintf(fid,'%0.4f\t',stats(jj));
+    
+    if isempty(histData)
+        fprintf(fid,'%s\t%d\t%.2f\t%d\t%s\t',imgName,sliceNum,0,0,NorT);
+        fprintf(fid,'\r\n');        
+    else
+        %for ii = 1:size(histData,1)
+        for ii = 1:1
+            fprintf(fid,'%s\t%d\t%.2f\t%d\t%s\t',imgName,sliceNum,histData(ii,1),histData(ii,2),NorT);
+            if ii == 1
+                for jj = 1:size(stats)
+                    fprintf(fid,'%0.4f\t',stats(jj));
+                end
             end
+            fprintf(fid,'\r\n');
         end
-        fprintf(fid,'\r\n');
-    end    
+    end
     fclose(fid);
 end
