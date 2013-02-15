e0a1fcffd759f1331d36e53945063644d4008fa6
Yuming Liu <liuyuming.lab@gmail.com>
2013-02-15 17:16:11 -0600

    format the m files to standard style using "ctrl + i"

--- ctFIRE/calc_fiberang2.m
+++ ctFIRE/calc_fiberang2.m
@@ -6,39 +6,38 @@ function[Fang] = calc_fiberang2(X,F,k)
 for fi=1:length(F)
     fv = F(fi).v;
     Lf = length(fv);
-%  ym: 03/12/12
-    if Lf <= k; 
+    %  ym: 03/12/12
+    if Lf <= k;
         
         v1 = fv(1);
         v2 = fv(end);
         x1 = X(v1,:);
-        x2 = X(v2,:); 
+        x2 = X(v2,:);
         angxz = atan( (x2(:,3)-x1(:,3))./(x2(:,1)-x1(:,1)+eps) );
         angxy = atan( (x2(:,2)-x1(:,2))./(x2(:,1)-x1(:,1)+eps) );
         
         Fang(fi).angle_xz   = repmat(angxz,1,k);
         Fang(fi).angle_xy   = repmat(angxy,1,k);
         
-    else 
+    else
         
         for j=1:Lf-k
             
-        %calculate angle orientation of fibers at each fv point
+            %calculate angle orientation of fibers at each fv point
             v1 = fv(j);
             v2 = fv(j+k);
             x1 = X(v1,:);
-            x2 = X(v2,:); 
-
+            x2 = X(v2,:);
+            
             Fang(fi).angle_xz(j)   = atan( (x2(:,3)-x1(:,3))./(x2(:,1)-x1(:,1)+eps) );
             Fang(fi).angle_xy(j)   = atan( (x2(:,2)-x1(:,2))./(x2(:,1)-x1(:,1)+eps) );
         end
-      % the last k points have the same angle  
-         Fang(fi).angle_xz(j+1:Lf)   = repmat(Fang(fi).angle_xz(j),1,k);
-         Fang(fi).angle_xy(j+1:Lf)   = repmat(Fang(fi).angle_xy(j),1,k);
+        % the last k points have the same angle
+        Fang(fi).angle_xz(j+1:Lf)   = repmat(Fang(fi).angle_xz(j),1,k);
+        Fang(fi).angle_xy(j+1:Lf)   = repmat(Fang(fi).angle_xy(j),1,k);
     end
-        
+    
 end
 
-   
 
-   
+
