dd025bd8204965e5d640defdbc909e69d8af22f5
Jeremy Bredfeldt <jeremybredfeldt@gmail.com>
2013-10-21 14:19:50 -0500

    Compute results for DFS and DSS

--- svmAnalysis.m
+++ svmAnalysis.m
@@ -95,31 +95,59 @@ disp(sprintf('mean specificity: %f',mean(spec)));
 %svmStruct.Bias - svmStruct.ScaleData.shift
 
 
-%hazard analysis
-%Cox Prop Hazard Fit to DFS
-survS = dfs;
-survSE = ~dfse;
-[b_1,logL_1,H,stats_1] = coxphfit(v,survS,'censoring',survSE);
+for i = 2:3
+    if i == 2
+        %hazard analysis
+        %Cox Prop Hazard Fit to DFS
+        survS = dfs;
+        survSE = ~dfse;
+        txt = 'DFS';
+        xpos = 60;
+    elseif i == 3
+        survS = dss;
+        survSE = ~dsse;
+        txt = 'DSS';
+        xpos = 90;
+    end
+    [b_1,logL_1,H,stats_1] = coxphfit(v,survS,'censoring',survSE);
 
-disp('DFS exponents:');
-fprintf('  sc1: %0.3f\n',exp(b_1));
-disp('DFS p values:');
-fprintf('  sc1: %0.3f\n',stats_1.p);
+    disp([txt ' exponents:']);
+    fprintf('  sc1: %0.3f\n',exp(b_1));
+    disp([txt ' p values:']);
+    fprintf('  sc1: %0.3f\n',stats_1.p);
 
-%Kaplan-Meyer Curvs
-v = logical(v);
-survPos = survS(v);
-survPosE = survSE(v);
-survNeg = survS(~v);
-survNegE = survSE(~v);
-[f,x,flo,fup] = ecdf(survPos,'censoring',survPosE,'function','survivor');
-figure(2);
-stairs(x,f,'LineWidth',2);
-hold on;
+    %Kaplan-Meyer Curvs
+    v = logical(v);
+    survPos = survS(v);
+    survPosE = survSE(v);
+    survNeg = survS(~v);
+    survNegE = survSE(~v);
+    [f,x,flo,fup] = ecdf(survPos,'censoring',survPosE,'function','survivor');
+    figure(i);
+    axes('LineWidth',5,'FontSize',18);
+    stairs(x,f,'LineWidth',5);
+    hold on;
 
-[f,x,flo,fup] = ecdf(survNeg,'censoring',survNegE,'function','survivor');
-stairs(x,f,'g','LineWidth',2); 
-legend('TACS3 Pos','TACS3 Neg');
-title('DFS KM Curve');
-xlabel('Months');
-ylabel('Survival Fraction');
+    [f,x,flo,fup] = ecdf(survNeg,'censoring',survNegE,'function','survivor');
+    stairs(x,f,'g','LineWidth',5); 
+    L = legend('TACS3 Pos','TACS3 Neg');
+    set(L, 'Box', 'off');
+    text(xpos,0.95,txt,'FontSize',24);
+    xlabel('Months');
+    ylabel('Survival Fraction');
+    ylim([0.5 1.0]);
+end
+
+%compute correlation between man and auto scores
+sc1 = survData(2:end,2);
+sc1 = double(vertcat(sc1{:}));
+sc2 = survData(2:end,3);
+sc2 = vertcat(sc2{:});
+sc3 = survData(2:end,4);
+sc3 = vertcat(sc3{:});
+
+[c1,p1] = corrcoef(v,sc1);
+[c2,p2] = corrcoef(v,sc2);
+[c3,p3] = corrcoef(v,sc3);
+fprintf('Cor sc1: %0.3f, sc2: %0.3f, sc3: %0.3f\n',c1(2),c2(2),c3(2));
+fprintf('CorP sc1: %0.6f, sc2: %0.6f, sc3: %0.6f\n',p1(2),p2(2),p3(2));
