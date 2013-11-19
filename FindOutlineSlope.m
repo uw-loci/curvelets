a4946e1b8eecc4340408b0d4f736528c3f51cd9e
Jeremy Bredfeldt <jeremybredfeldt@gmail.com>
2013-11-18 22:08:19 -0600

    Use the slope from the curve fit

--- FindOutlineSlope.m
+++ FindOutlineSlope.m
@@ -23,17 +23,14 @@ slope = NaN;
 %find the list of connected points on the outline that are
 %surrounding pt
 num = 21; %number of points to return
-[con_pts] = FindConnectedPts(boundaryMask, idx, num);
+[con_pts] = FindConnectedPts([boundaryMask(:,2) boundaryMask(:,1)], idx, num);
 
 if (isnan(con_pts(1,1)))
     return;
 end
 
-
-%plot(con_pts(:,1),con_pts(:,2),'yo');
-
-%fit a curve to these points, then compute floating point angle of tangent line
-
+%figure(500);
+plot(con_pts(:,1),con_pts(:,2),'yo');
 
 %compute absolute slope of the tangent
 %rise
@@ -53,16 +50,17 @@ if slope < 45 || slope > 135
     y_f = linspace(con_pts(1,2),con_pts(end,2),50);
     x_p = polyfit(con_pts(:,2),con_pts(:,1),2);
     x_f = polyval(x_p,y_f);
-%     figure(3);
-%     plot(x_f,y_f,'yo');
+    %figure(500);
+    plot(x_f,y_f,'r*','markersize',2);
 else
     %more unique points in horiz dir
     x_f = linspace(con_pts(1,1),con_pts(end,1),50);
     y_p = polyfit(con_pts(:,1),con_pts(:,2),2);
     y_f = polyval(y_p,x_f);
-%     figure(3);
-%     plot(x_f,y_f,'yo');
+    %figure(500);
+    plot(x_f,y_f,'r*','markersize',2);
 end
+
 warning on all;
 rise2 = x_f(26)-x_f(24);
 run2 = y_f(26)-y_f(24);
@@ -71,8 +69,8 @@ slope2 = (theta2*180/pi);
 if slope2<0
     slope2 = slope2+180;
 end
-%dif = slope2-slope;
-%slope = slope2;
+dif = slope2-slope;
+slope = slope2;
 
 end
 
