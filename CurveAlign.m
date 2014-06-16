88efa85ae06a848cc040ff7cde006182562a0714
Yuming Liu <liuyuming.lab@gmail.com>
2014-06-16 17:57:55 -0500

    add feature ranking button, an annotation.xlsx is needed to run this feature

--- CurveAlign.m
+++ CurveAlign.m
@@ -43,6 +43,8 @@ if ~isdeployed
 end
 
 global imgName
+global ssU   % screen size of the user's display
+ssU = get(0,'screensize');
 
 if exist('lastParams.mat','file')
     %use parameters from the last run of curveAlign
@@ -75,10 +77,17 @@ P(7:9,7:9) = 1*ones(3,3);
 
 guiCtrl = figure('Resize','on','Units','pixels','Position',[50 75 500 650],'Visible','off','MenuBar','none','name','CurveAlign V3.0','NumberTitle','off','UserData',0);
 guiFig = figure('Resize','on','Units','pixels','Position',[525 125 600 600],'Visible','off','MenuBar','none','name','CurveAlign Figure','NumberTitle','off','UserData',0);
+guiRank1 = figure('Resize','on','Units','normalized','Position',[0.30 0.35 0.68*ssU(4)/ssU(3) 0.55],'Visible','off','MenuBar','none','name','CA Features List','NumberTitle','off','UserData',0);
+guiRank2 = figure('Resize','on','Units','normalized','Position',[0.7 0.55 0.45*ssU(4)/ssU(3) 0.40],'Visible','off','MenuBar','none','name','Feature Normalized Difference (Pos-Neg)','NumberTitle','off','UserData',0);
+guiRank3 = figure('Resize','on','Units','normalized','Position',[0.7 0.10 0.45*ssU(4)/ssU(3) 0.40],'Visible','off','MenuBar','none','name','Feature Classification Importance','NumberTitle','off','UserData',0);
+
 
 defaultBackground = get(0,'defaultUicontrolBackgroundColor');
 set(guiCtrl,'Color',defaultBackground);
 set(guiFig,'Color',defaultBackground);
+set(guiRank1,'Color',defaultBackground);
+set(guiRank2,'Color',defaultBackground);
+set(guiRank3,'Color',defaultBackground);
 
 set(guiCtrl,'Visible','on');
 
@@ -109,6 +118,12 @@ imgLabel = uicontrol('Parent',guiCtrl,'Style','text','String','None Selected','H
 % button to select a boundary file
 %loadBoundary = uicontrol('Parent',guiCtrl,'Style','pushbutton','String','Get CSV','FontUnits','normalized','FontSize',.4,'UserData',[],'Units','normalized','Position',[.0 .76 .3 .05],'callback','ClickedCallback','Callback', {@boundIn});
 %boundLabel = uicontrol('Parent',guiCtrl,'Style','text','String','None Selected','Enable','off','HorizontalAlignment','left','FontUnits','normalized','FontSize',.18,'Units','normalized','Position',[.3 .70 .5 .1]);
+
+%% feature ranking button
+% button to process an output feature mat files
+fRanking = uicontrol('Parent',guiCtrl,'Style','pushbutton','String','Feature Ranking',...
+    'FontUnits','normalized','FontSize',.4,'UserData',[],'Units','normalized','Position',[.65 .78 .3 .05],...
+    'callback','ClickedCallback','Callback', {@featR});
 
 % button to run measurement
 imgRun = uicontrol('Parent',guiCtrl,'Style','pushbutton','String','Run','FontUnits','normalized','FontSize',.7,'Units','normalized','Position',[0 .0 .5 .05]);
@@ -157,13 +172,14 @@ makeMap = uicontrol('Parent',guiPanel,'Style','checkbox','Enable','off','String'
 slideLab = uicontrol('Parent',guiCtrl,'Style','text','String','Stack image selected:','Enable','off','FontUnits','normalized','FontSize',.18,'Units','normalized','Position',[0 .64 .75 .1]);
 stackSlide = uicontrol('Parent',guiCtrl,'Style','slide','Units','normalized','position',[0 .62 1 .1],'min',1,'max',100,'val',1,'SliderStep', [.1 .2],'Enable','off');
 
-infoLabel = uicontrol('Parent',guiCtrl,'Style','text','String','Choose methods, then click Get Image(s) button.','FontUnits','normalized','FontSize',.18,'Units','normalized','Position',[0 .1 .9 .1],'BackgroundColor','g');
+infoLabel = uicontrol('Parent',guiCtrl,'Style','text','String','Choose methods, then click Get Image(s) button; Or Click Feature Ranking for ranking CA extracted features.','FontUnits','normalized','FontSize',.18,'Units','normalized','Position',[0 .1 .9 .1],'BackgroundColor','g');
 
 % set font
-set([guiPanel keepLab1 keepLab2 distLab infoLabel enterKeep enterDistThresh makeValues makeRecon makeHist makeAssoc imgOpen imgRun imgReset slideLab],'FontName','FixedWidth')
+set([guiPanel keepLab1 keepLab2 distLab infoLabel enterKeep enterDistThresh makeValues makeRecon makeHist makeAssoc imgOpen fRanking imgRun imgReset slideLab],'FontName','FixedWidth')
 set([keepLab1 keepLab2 distLab],'ForegroundColor',[.5 .5 .5])
-set([imgOpen imgRun imgReset],'FontWeight','bold')
+set([imgOpen fRanking imgRun imgReset],'FontWeight','bold')
 set([keepLab1 keepLab2 distLab slideLab infoLabel],'HorizontalAlignment','left')
+
 
 %initialize gui
 set([imgRun makeHist makeRecon enterKeep enterDistThresh makeValues],'Enable','off')
@@ -393,6 +409,8 @@ note3 = 'boundary files must be in same dir as images and conform to naming conv
         
         set(imgRun,'Callback',{@runMeasure});
         set(imgOpen,'Enable','off');
+        set(fRanking,'Enable','off');
+        
         set([makeRecon makeHist makeValues makeFeat makeOver makeMap imgRun],'Enable','on');
         set([makeRecon makeHist makeValues],'Enable','off') % yl,default output
         %disable method selection
@@ -433,6 +451,294 @@ note3 = 'boundary files must be in same dir as images and conform to naming conv
         set(enterDistThresh,'UserData',usr_input)
     end
 
+%%--------------------------------------------------------------------------
+%callback function for postprocess button
+function featR(featRanking,eventdata)
+   
+    set(bndryModeDrop,'Enable','off');
+    set(fibModeDrop,'Enable','off');
+    set(imgOpen,'Enable','off');
+    set(infoLabel,'String','Feature Ranking is ongoing');
+
+    
+    fibFeatDir = uigetdir(pathNameGlobal,'Select Fiber feature Directory:');
+    % [fileName pathName] = uigetfile({'*.tif;*.tiff;*.jpg;*.jpeg';'*.*'},'Select Image',pathNameGlobal,'MultiSelect','on');
+     pathNameGlobal = fibFeatDir;
+     save('lastParams.mat','pathNameGlobal','keepValGlobal','distValGlobal');
+     
+    fibFeatDir = [fibFeatDir,'\'];
+    fileList = dir(fibFeatDir);
+    lenFileList = length(fileList);
+    feat_idx = zeros(1,lenFileList);
+
+    %Search for feature files
+    for i = 1:lenFileList
+        if ~isempty(regexp(fileList(i).name,'fibFeatures.mat', 'once', 'ignorecase'))
+            feat_idx(i) = 1;
+        end
+    end
+    featFiles = fileList(feat_idx==1);
+    lenFeatFiles = length(featFiles);
+    %Compile a big array of features in RAM
+    obsFileIdx = zeros(lenFeatFiles,1);
+    totFeat = 0;
+    for i = 1:lenFeatFiles
+        %Find how many observations are in each file
+        obsName = featFiles(i).name;
+        bff = [fibFeatDir obsName];
+        feat = load(bff);
+        [lenFeat widFeat] = size(feat.fibFeat);
+        totFeat = totFeat + lenFeat;
+        obsFileIdx(i) = totFeat;
+    end
+    %Allocate space for complete feature array (For all images)
+    compFeat = zeros(totFeat,widFeat+1);
+    compFeatMeta(lenFeatFiles) = struct('imageName',[],'topLevelDir',[],'fireDir',[],'outDir',[],'numToProc',[],'fibProcMeth',[],'keep',[],'distThresh',[]);
+
+    %Put together big array
+    prevTot = 1;
+    totFeat = 0;
+    for i = 1:lenFeatFiles
+        obsName = featFiles(i).name;
+        bff = [fibFeatDir obsName];
+        feat = load(bff);
+        [lenFeat widFeat] = size(feat.fibFeat);
+        totFeat = totFeat + lenFeat; %Pointer to last array position
+        compFeat(prevTot:totFeat,1:end-1) = feat.fibFeat; %Add to array
+        compFeat(prevTot:totFeat,end) = zeros(lenFeat,1)+i; %Add index into meta data array
+        compFeatMeta(i).imageName = feat.imgNameP;
+        prevTot = totFeat+1; %Pointer to first array position
+    end
+    featNames = feat.featNames;
+
+    %Save feature array and meta array to disk
+    compFeatFF = [fibFeatDir 'compFeat.mat'];
+    % save(compFeatFF,'compFeat','compFeatMeta');
+
+    %%
+    %Read from file just to check
+    temp = load(compFeatFF);
+    compFeat = temp.compFeat;
+    compFeatMeta = temp.compFeatMeta;
+
+    %% Train based on image annotations as a whole
+    %load files for image labels 
+%     labelMeta = [1 0 0 0 1 0 0 1 0 0 0 1 1 1 1 1]; %label for each of 16 training images (label image as a whole)
+% fibFeatDir2 = [pwd '\CurveAlignTestImages\TrainingSets20131113\CA_Out\'];
+% for i = 1:lenFeatFiles
+%     obsNameS{i} = featFiles(i).name;
+%     xlswrite([fibFeatDir2,'annotation.xlsx'], labelMeta(i),'sheet1',['A',num2str(i)]);
+%     xlswrite([fibFeatDir2,'annotation.xlsx'], {obsNameS{i}(1:end-16)},'sheet1',['B',num2str(i)]);
+%      
+% end
+% clear obsNameS
+
+  [labelMeta IMGname] = xlsread([fibFeatDir,'annotation.xlsx']);
+   labelMeta = labelMeta';
+
+
+    [lenFeat widFeat] = size(compFeat); %get size of the complete feature matrix (including meta index)
+    labelObs = zeros(lenFeat,1); %The label for each observation
+    for i = 1:lenFeat
+        labelObs(i) = labelMeta(compFeat(i,end)); %last col contains index to metaData
+    end
+    labelObs = logical(labelObs);
+
+    %show featurelist
+    figure(guiRank1);
+    fz1 = 9;
+    for i = 1:length(featNames)
+        if i<13
+            text(0,1-0.08*i,sprintf('%d: %s',i,featNames{i}),'fontsize',fz1);
+        elseif i > 12 && i < 25
+            text(0.34,1-0.08*(i-12),sprintf('%d: %s',i,featNames{i}),'fontsize',fz1);
+        elseif i > 24
+            text(0.68,1-0.08*(i-24),sprintf('%d: %s',i,featNames{i}),'fontsize',fz1);
+        end
+    end
+    axis(gca,'off')
+                
+
+        %1. fiber Key into CTFIRE list
+        %2. row
+        %3. col
+        %4. abs ang
+        %5. fiber weight
+        %6. total length
+        %7. end to end length
+        %8. curvature
+        %9. width
+        %10. dist to nearest 2
+        %11. dist to nearest 4
+        %12. dist to nearest 8
+        %13. dist to nearest 16
+        %14. mean dist
+        %15. std dist
+        %16. box density 32
+        %17. box density 64
+        %18. box density 128
+        %19. alignment of nearest 2
+        %20. alignment of nearest 4
+        %21. alignment of nearest 8
+        %22. alignment of nearest 16
+        %23. mean align
+        %24. std align
+        %25. box alignment 32
+        %26. box alignment 64
+        %27. box alignment 128
+        %28. nearest dist to bound
+        %29. inside epi region
+        %30. nearest relative boundary angle
+        %31. extension point distance
+        %32. extension point angle
+        %33. boundary point row
+        %34. boundary point col
+
+    posObs = compFeat(labelObs,:); %positive fiber observations
+    negObs = compFeat(~labelObs,:); %negative fiber observations
+
+    posM = nanmean(posObs); %average over observations
+    negM = nanmean(negObs);
+    posStd = nanstd(posObs);
+    negStd = nanstd(negObs);
+
+    compM = [posM; negM]; %composite matrix
+    compStd = [posStd; negStd];
+    maxM = nanmax(compM); %max between positive and neg
+    compMN(1,:) = compM(1,:)./maxM; %normalize
+    compMN(2,:) = compM(2,:)./maxM;
+    compStdN(1,:) = compStd(1,:)./maxM;
+    compStdN(2,:) = compStd(2,:)./maxM;
+   % YL add gui
+    % feats = [6 8:9 14:18 23:32]; %Best feature set
+    featsDef = '6, 8:9, 14:18, 23:32'; %Best feature set
+
+    name='Select features to be ranked';
+    % prompt= featNames';
+    prompt = {'Selected features'};
+    numlines=1;
+    defaultanswer= {featsDef};
+    % updatepnum = defaultanswer;[5 7 10 15:20];
+    % promptud = prompt(updatepnum);
+    %  defaultud=defaultanswer(updatepnum);
+    %     FIREp = inputdlg(prompt,name,numlines,defaultanswer);
+    options.Resize='on';
+    options.WindowStyle='normal';
+    options.Interpreter='tex';
+    featsIN = inputdlg(prompt,name,numlines,defaultanswer,options);
+
+    if length(featsIN)== 1
+        feats = str2num(featsIN{1});
+    else
+        disp('Please confirm or update the selected fiber features')
+        featsIN = 0;
+    end
+
+    
+    %feats = [28:32];
+    featNamesS = featNames(feats); %throw out names that are not included
+    lenSubFeats = length(feats);
+
+    %Now make each image an observation by calculating the mean over fibers
+    imgObsM = zeros(lenFeatFiles,widFeat);
+    for i = 1:lenFeatFiles    
+        imgObsM(i,:) = nanmean(compFeat(compFeat(:,end) == i,:));
+    end
+
+    %check to make sure these features can classify the training set
+    SVMStruct = svmtrain(imgObsM(:,feats),labelMeta,'showplot','true');
+    if lenSubFeats == 2
+        xlabel(featNamesS(1));
+        ylabel(featNamesS(2));
+    end
+    v = svmclassify(SVMStruct,imgObsM(:,feats));
+    labelMeta = labelMeta';
+    tp = sum((v(:,1) == labelMeta(:,1) & labelMeta(:,1) == 1));
+    tn = sum((v(:,1) == labelMeta(:,1) & labelMeta(:,1) == 0));
+    fp = sum((v(:,1) ~= labelMeta(:,1) & labelMeta(:,1) == 0));
+    fn = sum((v(:,1) ~= labelMeta(:,1) & labelMeta(:,1) == 1));
+    sens = tp/(tp+fn);
+    spec = tn/(fp+tn);   
+    disp(sprintf('mean sensitivity: %4.2f',mean(sens)));
+    disp(sprintf('mean specificity: %4.2f',mean(spec)));
+
+    %Plot normalized average feature values for each class
+    figure(guiRank2);
+    %set(gcf,'Position',[1 1 1000 750]);
+    difCompMN = compMN(1,:) - compMN(2,:);
+    difCompStd = sqrt(compStdN(1,:).^2 + compStdN(2,:).^2); % YL: compStdN(2,:).^2?
+    difMN = difCompMN(:,feats);
+    difStdS = difCompStd(:,feats);
+    [difS, idxS] = sort(difMN);
+    barh(difS);
+    featNamesS1 = featNamesS(idxS); %Sorted name list
+    difStdS = difStdS(idxS)./10; %shrink to make plotable
+    %xlim([0.2 1.0]);
+    set(gca,'YTick',1:lenSubFeats,'YTickLabel',featNamesS1);
+    set(gca,'XGrid','off','YGrid','on');
+    xlabel('Normalized Difference (Pos-Neg)');
+    for i = 1:lenSubFeats
+        %plot relative error
+        yp = lenSubFeats-i+1;
+        line([difS(i)-difStdS(yp) difS(i)+difStdS(yp)],[i i],'Color','g');
+    end
+    
+    
+    text(min(difS)-max(difStdS),lenSubFeats+2.85,sprintf('Sensitivity: %4.2f; Specificity: %4.2f',mean(sens),mean(spec)),'color','r');
+    
+    %Plot feature rank
+    wtSVM = SVMStruct.Alpha'*SVMStruct.SupportVectors;
+    absWt = wtSVM.^2;
+    [absWtS, idxS] = sort(absWt); %Sort based on importance
+    figure(guiRank3); barh(absWtS); %plot bar graph
+    %set(gcf,'Position',[1 1 1000 750]);
+    featNamesS = featNamesS(idxS); %sort feature names
+    set(gca,'YTick',1:lenSubFeats,'YTickLabel',featNamesS);
+    xlabel('Classification Importance');
+    text(min(absWtS),lenSubFeats+2.85,sprintf('Sensitivity: %4.2f; Specificity: %4.2f',mean(sens),mean(spec)),'color','r');
+
+    %Save rank to file
+%     fibFeatDir2 = pwd;
+    featRankFF = [fibFeatDir 'featRank.txt'];
+    fid = fopen(featRankFF,'w+');
+    difMNS = difMN(idxS);
+    for i = 1:lenSubFeats
+        fprintf(fid,'%d\t%s\t%f\t%f\r\n',i,featNamesS{i},absWtS(i),difMNS(i));
+    end   
+    fclose(fid);
+
+    %% Try to use each fiber as an observation
+%     compFeat(isnan(compFeat)) = 0;
+%     folds = 50;
+%     rndIdx = logical(round(rand(lenFeat,1)*(0.5+1/folds)));
+%     %train
+%     SVMStruct = svmtrain([compFeat(rndIdx,feats)],labelObs(rndIdx),'kernel_function','linear','showplot','true');
+%     if lenSubFeats == 2
+%         xlabel(featNamesS(1));
+%         ylabel(featNamesS(2));
+%     end
+%     v = svmclassify(SVMStruct,compFeat(:,feats));
+%     tp = sum((v(:,1) == labelObs(:,1) & labelObs(:,1) == 1));
+%     tn = sum((v(:,1) == labelObs(:,1) & labelObs(:,1) == 0));
+%     fp = sum((v(:,1) ~= labelObs(:,1) & labelObs(:,1) == 0));
+%     fn = sum((v(:,1) ~= labelObs(:,1) & labelObs(:,1) == 1));
+%     sens = tp/(tp+fn);
+%     spec = tn/(fp+tn);   
+%     disp(sprintf('mean sensitivity: %f',mean(sens)));
+%     disp(sprintf('mean specificity: %f',mean(spec)));
+% 
+%     wtSVM = SVMStruct.Alpha'*SVMStruct.SupportVectors;
+%     absWt = wtSVM.^2;
+%     figure(300); barh(absWt);
+
+%      set([makeRecon makeHist makeValues makeFeat makeOver makeMap imgRun],'Enable','on');
+%      set([makeRecon makeHist makeValues],'Enable','off') % yl,default output
+   
+  
+%    set(fibModeDrop,'Enable','off');
+   set(infoLabel,'String','Feature Ranking is done');
+
+end  % featR
 %--------------------------------------------------------------------------
 % callback function for imgRun
     function runMeasure(imgRun,eventdata)
