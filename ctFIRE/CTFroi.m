dccc8fecb30dcdb53ddffd1d6b743c6728111b42
guneetsinghmehta <mehta_guneet@iitj.ac.in>
2015-10-12 14:02:14 -0500

    Saving the automatically detected ROI

--- ctFIRE/CTFroi.m
+++ ctFIRE/CTFroi.m
@@ -2610,12 +2610,16 @@ function[]=CTFroi(ROIctfp)
                     end
                     if(property_column==3)
                         figure(image_fig);text(x_max,y_max-8,'Max length','Color',[1 1 0]);
+                        fieldname='max_length';
                     elseif(property_column==4)
                         figure(image_fig);text(x_max,y_max-8,'Max Width','Color',[1 1 0]);
+                        fieldname='max_width';
                     elseif(property_column==5)
                         figure(image_fig);text(x_max,y_max-8,'Max angle','Color',[1 1 0]);
+                        fieldname='max_angle';
                     elseif(property_column==6)
                         figure(image_fig);text(x_max,y_max-8,'Max straightness','Color',[1 1 0]);
+                        fieldname='max_straightness';
                     end
                     toc;
 
@@ -2628,6 +2632,21 @@ function[]=CTFroi(ROIctfp)
                          boundary = B{k2};
                          plot(boundary(:,2), boundary(:,1), 'y', 'LineWidth', 2);%boundary need not be dilated now because we are using plot function now
                       end
+                      
+                      %saving the ROI now
+                      separate_rois.(fieldname).roi=[a,b,window_size,window_size];
+                      c=clock;fix(c);
+                        date=[num2str(c(2)) '-' num2str(c(3)) '-' num2str(c(1))] ;% saves 20 dec 2014 as 12-20-2014
+                        separate_rois.(fieldname).date=date;
+                        time=[num2str(c(4)) ':' num2str(c(5)) ':' num2str(uint8(c(6)))]; % saves 11:50:32 for 1150 hrs and 32 seconds
+                        separate_rois.(fieldname).time=time;
+                        separate_rois.(fieldname).shape=roi_shape;
+                        separate_rois.(fieldname).xm=x_max;
+                        separate_rois.(fieldname).ym=y_max;
+                        separate_rois.(fieldname).enclosing_rect=[a,b,a+window_size,b+window_size];
+                        save(fullfile(pathname,'ROI\ROI_management\',[filename,'_ROIs.mat']),'separate_rois','-append'); 
+                        update_rois;
+                      
             elseif(use_defined_rois==1)
                 % only for simple ROIs and not combined ROIs
                 Data=get(roi_table,'Data');
