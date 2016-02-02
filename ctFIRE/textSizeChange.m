f3bf04942700a1d082e2f88302f3912d020aa6c8
guneetsinghmehta <mehta_guneet@iitj.ac.in>
2016-02-02 16:19:00 -0600

    change of textSizeChange script
    calling the textSizeChange script from ctFIRE

--- ctFIRE/textSizeChange.m
+++ ctFIRE/textSizeChange.m
@@ -1,4 +1,7 @@
+% takes the screen size - finds the height of the screen divides by a ratio
+% of 768/8 and scales up the text by the same amount
 fig=gcf;
-ratio=1366/8;
-newTextSize=floor(max(get(0,'screensize'))/ratio);
-set(findall(fig,'-property','FontSize'),'FontSize',8);
+SS=get(0,'screensize');
+ratio=768/8;%768 is based on a computer
+newTextSize=floor(SS(4)/ratio);
+set(findall(fig,'-property','FontSize'),'FontSize',newTextSize);
