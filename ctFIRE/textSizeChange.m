9fbd906bdb1ea5f418b8042893b4f846349bea4e
guneetsinghmehta <mehta_guneet@iitj.ac.in>
2016-02-02 16:32:56 -0600

    removing fontunits normalization condition changing font size of the figure depending on screen height
    changing the textSizeChange to a function instead of a script

--- ctFIRE/textSizeChange.m
+++ ctFIRE/textSizeChange.m
@@ -1,7 +1,9 @@
 % takes the screen size - finds the height of the screen divides by a ratio
 % of 768/8 and scales up the text by the same amount
-fig=gcf;
-SS=get(0,'screensize');
-ratio=768/8;%768 is based on a computer
-newTextSize=floor(SS(4)/ratio);
-set(findall(fig,'-property','FontSize'),'FontSize',newTextSize);
+function[]=textSizeChange(figNumber)
+    fig=figNumber;
+    SS=get(0,'screensize');
+    ratio=768/8;%768 is based on a computer
+    newTextSize=floor(SS(4)/ratio);
+    set(findall(fig,'-property','FontSize'),'FontSize',newTextSize);
+end
