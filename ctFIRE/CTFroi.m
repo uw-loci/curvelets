755a0d06026e7b72499b9520be22ad25da4d0ca3
yuminguw <liu372@wisc.edu>
2016-12-30 17:33:04 -0600

    Use roipoly to create mask instead of createMask to avoid an error of ambiguous syntax

--- ctFIRE/CTFroi.m
+++ ctFIRE/CTFroi.m
@@ -569,8 +569,9 @@ function[]=CTFroi(ROIctfp)
             data2=roi;
             a=data2(1);b=data2(2);c=data2(3);d=data2(4);
             [s1,s2]=size(image);
-             BW = createMask(h);
+%              BW = createMask(h,image_fig); % Ambiguous syntax. Associated axes contains more than one image.
              vertices = getVertices(h);
+            BW=roipoly(image,vertices(:,1),vertices(:,2)); % replace createMask
             x_min=a;x_max=a+c;y_min=b;y_max=b+d;
             x_min=floor(x_min);x_max=floor(x_max);y_min=floor(y_min);y_max=floor(y_max);
         elseif(roi_shape==4)%polygon
