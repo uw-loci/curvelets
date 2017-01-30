494a2c8c3c618084fe653ba040939fad0006576c
yuminguw <liu372@wisc.edu>
2017-01-29 20:45:43 -0600

    Check in "checkACoutput" and add stack check for both checkCAoutput and checkCTFoutput

--- checkCAoutput.m
+++ checkCAoutput.m
@@ -1,41 +1,41 @@
-function ctfFnd = checkCTFoutput(pathName, fileName)
-% Check the CT-FIRE output files in "ctFIREout" folder.
-% Returns a cell array of CT-FIRE file names.
-% Naming convention: ctFIREout_[Image Name wout ext].mat
+function CAFnd = checkCAoutput(pathName, fileName)
+% Check the CA output files in "CA_Out" folder.
+% Returns a cell array of CA output file names.
+% Naming convention: [imgName '*fibFeatures.MAT']
 %
 % Inputs
 %    pathname = path to the current set of images
 %    fileName = CELL array of filenames    
 %    
 % Outputs
-%    ctfFnd = CELL array of found CT-FIRE output files, empty if no file is
+%    CAFnd = CELL array of found CA output files, empty if no file is
 %    found
 %    
 % modified from checkCTFiles.m in CurveAlign implementation
 
     %list out current directory
     numImgs = length(fileName);   
-    %preallocate space for ctFire files cell array
-    ctfFnd = cell(numImgs,1);   
+    %preallocate space for CA files cell array
+    CAFnd = cell(numImgs,1);   
     for i = 1:numImgs
         fndFlag = 0; %reset found flag
         %parse out only filename, without extension
-         [~, imgName, ~] = fileparts(fileName{i});
+        [~, imgName, ~] = fileparts(fileName{i});
         iteminfo = imfinfo(fullfile(pathName,fileName{i}));
         numSections = numel(iteminfo);
         if numSections == 1
-            ctFireName = ['OL_ctFIRE_' imgName '*.tif'];
+            FEAdataName = [imgName '_fibFeatures.mat'];
         elseif numSections > 1
-            ctFireName = ['OL_ctFIRE_' imgName '_s*.tif'];
+            FEAdataName = [imgName '_s*_fibFeatures.mat'];
         end
         %search directory for a file with correct naming convention 
-         OLfiles = dir(fullfile(pathName,'ctFIREout',ctFireName));
-         if ~isempty(OLfiles)
-                ctfFnd{i,1} = OLfiles(1).name;
+         FEAfiles = dir(fullfile(pathName,'CA_Out',FEAdataName));
+         if ~isempty(FEAfiles)
+                CAFnd{i,1} = FEAfiles(1).name;
                 fndFlag = 1;
          end
          if fndFlag == 0  %missing a ctfire overlay image,
-            ctfFnd{i} = {};
+            CAFnd{i} = {};
          end
     end
 
