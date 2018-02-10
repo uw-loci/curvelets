e9838eb169ad45a5943090c114b8a27d1de3822c
Futurist4Science <31500701+Futurist4Science@users.noreply.github.com>
2018-02-09 18:21:11 -0600

    create new function file for project

---
+++ preprocmodule.m
@@ -0,0 +1,23 @@
+function [outputArg1,outputArg2] = preprocmodule(inputArg1,inputArg2)
+%PREPROCMODULE function summary: GUI Module for Image Preprocessing
+%   GUI Module with image processing methods to prepare microscopy images
+%   for CurveAlign/CT-FIRE Collagen fiber analysis methods.
+%
+%Input:
+%   Args...
+%
+%Log:
+%   1. January 2018 to May 2018: Andrew S. Leicht student at UW-Madison was
+%   tasked with this project, under the supervision of Yuming Liu at LOCI.
+%   Development was done with Matlab R2017b (9.3.0.713579) under Achedemic
+%   license on Ubuntu 16.04 LTS.
+%
+% Licensed under the 2-Clause BSD license 
+% Copyright (c) 2009 - 2018, Board of Regents of the University of Wisconsin-Madison
+% All rights reserved.
+
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
