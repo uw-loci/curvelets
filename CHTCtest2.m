0c952a2b2ba012bc8249befb1b567c0b83ebe1fe
yuminguw <liu372@wisc.edu>
2018-02-12 14:32:34 -0600

    Update current CHTC test file setting

--- CHTCtest2.m
+++ CHTCtest2.m
@@ -1,7 +1,7 @@
 CTFP_file = 'CTFP_cluster.txt';
 CAP_file = 'CAP_cluster.txt';
 CAroiP_file = 'CAroiP_cluster.txt';
-jobtarfile = '1045(-)1v2_Job-1.tar';
+jobtarfile = '1045(-)1v2_Job-1git.tar';
 imgFormat = '.tif';
 
 % LOCIca_cluster(CTFP_file,CAP_file,CAroiP_file,jobtarfile,imgFormat)
