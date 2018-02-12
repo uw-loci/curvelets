1f558f3a318bd18d8a4751f607044841c290f21b
yuminguw <liu372@wisc.edu>
2018-02-12 11:24:24 -0600

    CHTC test code for current data

--- CHTCtest2.m
+++ CHTCtest2.m
@@ -1,9 +1,9 @@
 CTFP_file = 'CTFP_cluster.txt';
 CAP_file = 'CAP_cluster.txt';
 CAroiP_file = 'CAroiP_cluster.txt';
-jobtarfile = '1045(-)1v2_Job-8.tar';
+jobtarfile = '1045(-)1v2_Job-1.tar';
 imgFormat = '.tif';
 
 % LOCIca_cluster(CTFP_file,CAP_file,CAroiP_file,jobtarfile,imgFormat)
-LOCIca_cluster(jobtarfile,imgFormat,'3');
+LOCIca_cluster(jobtarfile,imgFormat);
 
