9ce6c5599ee03538bd70725d668fa6240e479c74
yuminguw <liu372@wisc.edu>
2018-03-15 11:45:20 -0500

    Remove the ROI that is incomplete due to e.g. small size in the ROI mask

--- CAroi.m
+++ CAroi.m
@@ -1946,17 +1946,39 @@ function [] = CAroi(CApathname,CAfilename,CAdatacurrent,CAcontrol)
         % cell type
         if ~iscell(mask_filename_all)
            mask_filename_all = {mask_filename_all}; 
+           set(status_message,'String', 'loading ROI mask is failed as no mask file is selected  ')
         end
+        set(status_message,'String', 'Loading ROI mask ... ')
+
         for j=1:size(mask_filename_all,2)        
             mask_filename=mask_filename_all{1,j};
             mask_image=imread([mask_pathname mask_filename]);
             boundaries=bwboundaries(mask_image);%bwboundaries needed because no info on bounary in ROI database
+            emptyROI_ind = [];  %index to empty ROIs
+            emptyROI_num = 0;  % number of empty ROIs
             for i=1:size(boundaries,1)
                 boundaries_temp=boundaries{i,1};
-                BD_to_roi_fn(boundaries_temp);
+                try
+                    BD_to_roi_fn(boundaries_temp);
+                catch errBD2ROI
+                    emptyROI_num = emptyROI_num + 1;
+                    emptyROI_ind(emptyROI_num) = i;
+                    fprintf('Loading error for ROI %d out of %d: %s \n',i,size(boundaries,1),errBD2ROI.message);
+                end
+            end
+            % remove empty/incomplete ROI
+            if emptyROI_num > 0
+                fieldnames_all = fieldnames(separate_rois);
+                fprintf('The following region(s) is(are) empty or incomplete and will not be displayed in the ROI list: \n')
+                for i = 1:emptyROI_num
+                    fprintf('%d: %s ;',i, fieldnames_all{emptyROI_ind(i)});
+                end
+                separate_rois = rmfield(separate_rois,fieldnames_all{emptyROI_ind});
+                fprintf('\n')
             end
             save(fullfile(ROImanDir,[filename,'_ROIs.mat']),'separate_rois','-append'); 
             update_rois;
+            set(status_message,'String', 'Loading ROI mask is done.')
         end
        
     end
