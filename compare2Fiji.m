9938da638c0904358300e1b447c320092710a7ed
Futurist4Science <31500701+Futurist4Science@users.noreply.github.com>
2018-04-19 03:45:36 -0500

    Create function to compare converted images between Matlab and Fiji

---
+++ compare2Fiji.m
@@ -0,0 +1,9 @@
+function [] = compare2Fiji(ImagMatlab,FijiImag)
+% Define compare2Fiji function to compare Matlab converted to 8Bit files with the same image in
+% Fiji
+%Steps
+%   1. Read in both files as pixel intensity maps
+%   2. Compare the pixel intensities for differences
+%   3. Report any differences
+
+end
