ad18d4f89574e7feb70d702755d957172f7edeed
Futurist4Science <31500701+Futurist4Science@users.noreply.github.com>
2018-05-11 01:31:46 -0500

    Revamp thresholding methods and comments, code ISO-data Method, show threshold

--- pmAutoThresh.m
+++ pmAutoThresh.m
@@ -36,15 +36,14 @@ info = imfinfo(ff); % store tif meta-data tags
 numSections = numel(info); % # of images in stack
 outputFileName = [fileName '_Mask' fileExtension]; % setup output filename
 outputFullPath = fullfile(OutputFolder,outputFileName); % setup full output path
-ThreshMethFlag = 1;%set flag to select threshold method
-% (1) Global Otsu method; (2) Multilevel Otsu Method;
-% (3) Fixed threshold chosen with minimax principle; (4) Fixed-form
-% threshold yielding minimax performance multiplied by a small factor
-Mthreshlvl = 2;%setup number of threshold bins (n+1 levels segmentation) for Otsu multiple threshold method
-%have above levels user configurable with input or auto optimized with test here?
-% test if file already exists and overwrite first before appending
+ThreshMethFlag = 4;%set flag to select threshold method
+% (1) Global Otsu Method; (2) Local Otsu Method;
+% (3) Kittler-Illingworth Cluster Method;
+% (4) Ridler-Calvard (ISO-data) Cluster Method; (5) Kapur Entropy Method;
+% (6) Local Sauvola Method; (7)Local Adaptive Method
+
 %Internal Functions (Methods) to simplfy code
-    function I = AthreshInternal(ImgOri) %function to threshold an image with many options
+    function [thresh,I] = AthreshInternal(ImgOri) %function to threshold an image with many options
         switch ThreshMethFlag
             case 1 %3. Use Global Otsu Method to threshold images
                 [thresh,EM] = graythresh(ImgOri);
@@ -55,24 +54,35 @@ Mthreshlvl = 2;%setup number of threshold bins (n+1 levels segmentation) for Ots
                     fprintf('Automatic Image Thresholding done with %f Effectiveness Metric.\n',EM)
                 end
                 drawnow
-            case 2 %3. Use Multilevel Otsu Method to threshold image with (Mthreshlvl+1) levels
-                [thresh,EM]= multithresh(ImgOri,Mthreshlvl);
-                seg_I = imquantize(ImgOri,thresh);
-                I = im2bw(mat2gray(seg_I));%output binary mask
-                if numSections > 1
-                    fprintf('Automatic Image Thresholding done with a %f Effectiveness Metric for slice %u.\n',EM,S)
-                else
-                    fprintf('Automatic Image Thresholding done with %f Effectiveness Metric.\n',EM)
+            case 2 %3. Use Local Otsu Method to threshold image
+            case 3 %3. Use Kittler-Illingworth Cluster threshold method
+            case 4 %3. Use Ridler-Calvard (ISO-data) Cluster threshold method
+                vImgOri = ImgOri(:); % vectorize image matrix
+                [PixCt,PixInt] = imhist(vImgOri); % histogram of image
+                %setup formula for iterated threshold
+                T(1) = round(sum(PixCt .* PixInt) ./ sum(PixCt));
+                Change = 1;
+                i=1;	%counter of iterations of ThreshItr
+                while (Change ~= 0) && (i<15)
+                    T_indexes = find(PixInt >= T(i)); % sort Pixel Intensity bins
+                    T_i = T_indexes(1);	% finds the value (in "intensity") that is closest to the threshold.
+                    % calculate mean below current threshold: MBT
+                    MBT = sum(PixCt(1:T_i) .* PixInt(1:T_i) ) ./ sum(PixCt(1:T_i));
+                    % calculate mean above current threshold: MAT
+                    MAT = sum(PixCt(T_i:end) .* PixInt(T_i:end) ) ./ sum(PixCt(T_i:end));
+                    % Iterate new threshold as mean of MAT and MBT
+                    i= i+1;
+                    T(i) = round((MBT + MAT) ./ 2);
+                    Change = T(i) - T(i-1);
                 end
-                drawnow
-            case 3 %3. Fixed threshold chosen with minimax principle
-                thresh = thselect(ImgOri,'minimaxi');
-                seg_I = imquantize(ImgOri, thresh);
-                I = im2bw(mat2gray(seg_I));%output binary mask
-            case 4 %3. Fixed-form threshold yielding minimax performance multiplied by a small factor
-                thresh = thselect(ImgOri,'sqtwolog');
-                seg_I = imquantize(ImgOri, thresh);
-                I = im2bw(mat2gray(seg_I));%output binary mask
+                threshi = T(i);
+                % Normalize threshold to interval [0,1]
+                thresh = (threshi - 1) / (PixInt(end) - 1); 
+                I = im2bw(ImgOri,thresh);%output as binary mask
+            case 5 %3. Use Kapur Entropy threshold method
+            case 6 %3. Use Local Sauvola threshold method
+            case 7 %3. Use Local Adaptive threshold method
+                
         end
     end
 %If threshold mask file already exists delete it before proceeding
@@ -83,12 +93,14 @@ end
 if numSections > 1  %For case of multi-image stack
     for S = 1:numSections
         ImgOri = imread(ff,S,'Info',info);%2. read in image slicewise
-        I = AthreshInternal(ImgOri);
+        [thresh,I] = AthreshInternal(ImgOri);
+        fprintf('Threshold value found to be %f for slice %u.\n',thresh,S)
         imwrite(I, outputFullPath, 'WriteMode', 'append', 'Compression','none');%4. write slice to file
     end
 else %for case when tif is single image
     ImgOri = imread(ff);%2. read in image
-    I = AthreshInternal(ImgOri);
+    [thresh,I] = AthreshInternal(ImgOri);
+    fprintf('Threshold value found to be %f.\n',thresh)
     imwrite(I, outputFullPath, 'WriteMode', 'overwrite', 'Compression','none');%4. write to file
 end
 return;
