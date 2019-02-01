00e467d5d9b4dc8a32743ef5cf01d2ff1af63c4c
yuminguw <liu372@wisc.edu>
2019-02-01 13:20:08 -0600

    testing

--- test_LOCIca_cluster.m
+++ test_LOCIca_cluster.m
@@ -2,8 +2,10 @@
 clear,clc,home;
 % create parameter file file
 % jobtarfile = '1045(-)1v2_Job-1test.tar';
-jobtarfile = 'CHTC_testJob-3.tar';
+% jobtarfile = 'CHTC_testJob-3.tar';
+jobtarfile = 'test2.tar';
+
 imageextension = '.tif';
-AnalysisMode = '1';
+AnalysisMode = '2';
 
 LOCIca_cluster(jobtarfile,imageextension,AnalysisMode)
