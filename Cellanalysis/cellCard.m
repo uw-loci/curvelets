fdac37c220438f1eb04869cbdd67347c9d52d751
Heqiao Zhu <hzhu254@wisc.edu>
2021-08-25 16:17:31 -0500

    Minor performance issue

--- Cellanalysis/cellCard.m
+++ Cellanalysis/cellCard.m
@@ -90,30 +90,32 @@ load('details.mat','details');
 coord = details.coord(index,:,:);
 sizePolygon = size(coord);
 
-minX = coord(1,1,1);
-minY = coord(1,2,1);
-maxX = 0;
-maxY = 0;
-
 for i=1:sizePolygon(3)
-    X = coord(1,1,i);
-    Y = coord(1,2,i);
-    if X < minX
-        minX = X;
-    end
-    if X > maxX
-        maxX = X;
-    end
-    if Y < minY
-        minY = Y;
-    end
-    if Y > maxY
-        maxY = Y;
-    end
+    X(i) = coord(1,1,i);
+    Y(i) = coord(1,2,i);
 end
 
-sideX = maxX - minX;
-sideY = maxY - minY;
+pe = pyenv;
+
+pathToRect = fileparts(which('findAxisPoints.py'));
+if count(py.sys.path,pathToRect) == 0
+    insert(py.sys.path,int32(0),pathToRect);
+end
+
+load('rect.mat','rect');
+
+py.findAxisPoints.findRectPoints(X, Y);
+
+X1 = rect(1,1);
+X2 = rect(2,1);
+X3 = rect(3,1);
+Y1 = rect(1,2);
+Y2 = rect(2,2);
+Y3 = rect(3,2);
+
+sideX = sqrt((X2 - X1)^2 + (Y2 - Y1)^2);
+sideY = sqrt((X3 - X2)^2 + (Y3 - Y2)^2);
+
 
 boundingBoxArea = sideX * sideY;
 extent = area / boundingBoxArea;
