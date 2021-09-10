f156fbb05741fb4f64a4f476cd0b64ee1839a9df
Heqiao Zhu <hzhu254@wisc.edu>
2021-09-09 22:41:26 -0500

    Added the skimage image analysis kit, which is used in cellprofier to analysis masks

---
+++ Cellanalysis/cellPropertyTest.m
@@ -0,0 +1,27 @@
+function cellPropertyTest(number)
+
+load('labels.mat','labels');
+sizeLabels = size(labels);
+
+for i=1:sizeLabels(1)
+    for j=1:sizeLabels(2)
+        if labels(i,j) == number
+            labels(i,j) = 1;
+        else
+            labels(i,j) = 0;
+        end
+    end
+end
+
+save('fortest.mat','labels')
+
+pe = pyenv;
+
+pathToStardist = fileparts(which('CellPropertyTest.py'));
+if count(py.sys.path,pathToStardist) == 0
+    insert(py.sys.path,int32(0),pathToStardist);
+end
+
+py.CellPropertyTest.CellPropertyTest();
+
+end
--- Cellanalysis/saveTif.m
+++ Cellanalysis/saveTif.m
@@ -7,8 +7,8 @@ labels = double(labels);
 
 imwrite(labels,'mask_visual.tif');
 
-% graph(1)
-saveTifLightGray()
+graph(100)
+% saveTifLightGray()
 
 end
 
@@ -60,16 +60,16 @@ if count(py.sys.path,pathToRect) == 0
     insert(py.sys.path,int32(0),pathToRect);
 end
 
-py.findAxisPoints.findRectPoints(Y, X);
+py.findAxisPoints.findRectPoints(Y,X);
 
 load('rect.mat','rect');
 
 figure
 imshow('mask_visual.tif')
 hold on
-% for i=1:(sizeCoord(3)-1)
-%     plot([Y(i); Y(i+1)], [X(i); X(i+1)],'LineWidth',3,'Color','Red')
-% end
+for i=1:(sizeCoord(3)-1)
+    plot([Y(i); Y(i+1)], [X(i); X(i+1)],'LineWidth',3,'Color','Red')
+end
 % plot(details.points(:,2), details.points(:,1),'r.','MarkerSize', 10)
 % plot(rect(1,1), rect(1,2),'r.','MarkerSize', 10)
 % plot(rect(2,1), rect(2,2),'r.','MarkerSize', 10)
@@ -80,10 +80,10 @@ plot([rect(2,1); rect(3,1)], [rect(2,2); rect(3,2)],'LineWidth',2,'Color','Red')
 hold off
 
 o = atan((rect(3,2)-rect(2,2))/(rect(3,1)-rect(2,1))) * 180/pi;
-disp(rect(3,2))
-disp(rect(2,2))
-disp(rect(3,1))
-disp(rect(2,1))
-disp(o)
+% disp(rect(3,2))
+% disp(rect(2,2))
+% disp(rect(3,1))
+% disp(rect(2,1))
+% disp(o)
 
 end
