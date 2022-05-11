d1204de6a0ad60cbe08eaf53970f6c93a389a1de
Yuming Liu <liu372@wisc.edu>
2022-05-11 13:48:13 -0500

    Set the pyenv enviroment
    As cellpose and deepcell were not be able be installed in the same environment, switching environment was needed. To make it work, Matlab usually needs a reboot.

--- Cellanalysis/WholeCell/wholeCellLink.m
+++ Cellanalysis/WholeCell/wholeCellLink.m
@@ -1,16 +1,19 @@
 function wholeCellLink(image,model)
 
-pe = pyenv;
-
+%pe = pyenv;
 pathToStardist = fileparts(which('cellpose_seg.py'));
 if count(py.sys.path,pathToStardist) == 0
     insert(py.sys.path,int32(0),pathToStardist);
 end
 
 if strcmp(model,'Cellpose')
-    py.cellpose_seg.cyto_seg(image)
+%     terminate(pyenv)
+%     pyenv('Version','/Users/ympro/opt/anaconda3/envs/SDpy38/bin/python')
+    py.cellpose_seg.cyto_seg(image);
 elseif strcmp(model,'DeepCell') 
-    py.deepcell_seg.cyto_seg(image)
+%     terminate(pyenv)
+%     pyenv('Version','/Users/ympro/opt/anaconda3/envs/deepcell/bin/python')
+    py.deepcell_seg.cyto_seg(image);
 end
 
 end
