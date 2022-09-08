0000c92d97aaf49184bff00c03907c34128f84eb
Yuming Liu <liu372@wisc.edu>
2022-09-08 16:55:53 -0500

    Update autoThresh.m
    formatting

--- preporocessing/autoThresh.m
+++ preporocessing/autoThresh.m
@@ -1,6 +1,5 @@
 function autoThresh
-%UNTITLED2 Summary of this function goes here
-%   initializes model and controller
+%initializes model and controller
 
 model = autoThreshModel();     % initialize the model
 controller = autoThreshController(model);  % initialize controller
