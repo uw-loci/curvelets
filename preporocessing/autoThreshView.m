0ac7a98c4f191c666c74916b79952b3549a08da7
Yuming Liu <liu372@wisc.edu>
2022-09-09 16:36:36 -0500

    Update autoThreshView.m
    Add listener for the three main parameters of the model

--- preporocessing/autoThreshView.m
+++ preporocessing/autoThreshView.m
@@ -12,12 +12,12 @@ classdef autoThreshView < handle
             obj.autoThreshModel = autoThreshController.autoThreshModel;
             obj.gui = autoThreshGUI('controller',obj.autoThreshController);
             
-%             addlistener(obj.autoThreshModel,'conv8bit','PostSet', ...
-%                 @(src,evnt)view.handlePropEvents(obj,src,evnt));
-%             addlistener(obj.autoThreshModel,'flag','PostSet', ...
-%                 @(src,evnt)view.handlePropEvents(obj,src,evnt));
-%             addlistener(obj.autoThreshModel,'blackBcgd','PostSet', ...
-%                 @(src,evnt)view.handlePropEvents(obj,src,evnt));
+            addlistener(obj.autoThreshModel,'conv8bit','PostSet', ...
+                @(src,evnt)autoThreshView.handlePropEvents(obj,src,evnt));
+            addlistener(obj.autoThreshModel,'flag','PostSet', ...
+                @(src,evnt)autoThreshView.handlePropEvents(obj,src,evnt));
+            addlistener(obj.autoThreshModel,'blackBcgd','PostSet', ...
+                @(src,evnt)autoThreshView.handlePropEvents(obj,src,evnt));
 
         end
      end
@@ -25,14 +25,19 @@ classdef autoThreshView < handle
        methods (Static)
         function handlePropEvents(obj,src,evnt)
             evntobj = evnt.AffectedObject;
-            handles = guidata(obj.gui);
+            handles = obj.gui;
             switch src.Name
                 case 'conv8bit'
-                    set(handles.conv8bit, 'String', evntobj.conv8bit);
+                    set(handles.convTo8BitCheck, 'Value', evntobj.conv8bit);
                 case 'flag'
-                    set(handles.flag, 'String', evntobj.flag);
+                    set(handles.globalList, 'Value', evntobj.thresholdOptions_Global{evntobj.flag});
                 case 'blackBcgd'
-                    set(handles.blackBcgd,'String',evntobj.blackBcgd);
+                    set(handles.blackBackgroundCheck,'Value',evntobj.blackBcgd);
+                    if handles.blackBackgroundCheck.Value == 1
+                        handles.msgWindow.Value = [handles.msgWindow.Value;{'Image has a back background'}];
+                    else
+                        handles.msgWindow.Value = [handles.msgWindow.Value;{'Image has a white background'}];
+                    end
             end
         end
     end
