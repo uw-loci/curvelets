5e25a4b7b268d47a908cd24e0fa4bd52af88020c
Yuming Liu <liu372@wisc.edu>
2022-09-09 16:33:34 -0500

    Update autoThresh.m
    Initialize the model and the controller

--- preporocessing/autoThresh.m
+++ preporocessing/autoThresh.m
@@ -1,6 +1,9 @@
 function autoThresh
-%initializes model and controller
-
+%Initialize model and controller for the auto-threshold module
+h = findall(0,'Type','figure','Tag','autothreshold_gui');
+if ~isempty(h)
+    delete(h)
+end
 model = autoThreshModel();     % initialize the model
 controller = autoThreshController(model);  % initialize controller
 
