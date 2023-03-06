56672c525a1b85304cc3cbbb5bc56fdbd54136c2
Tim Liang <tliang36@wisc.edu>
2023-03-06 12:18:45 -0600

    Add if-else statements to support mex file compilation on mac and windows
    - On MATLAB 2022b
    - For mac, openmp must be installed. Instructions are on https://mac.r-project.org/openmp/
    - For windows, openmp is already installed and no changes need to be made.

--- ctFIRE/CPP/Link_Fibre.h
+++ ctFIRE/CPP/Link_Fibre.h
@@ -1,6 +1,11 @@
 #include <vector>
 #include <array>
-#include <omp.h>
+#if __APPLE__
+    #include "/usr/local/include/omp.h"
+#elif _WIN64
+    #include omp.h
+#endif
+
 // Matlab is colomn major.
 template<typename T,int d>
 struct Fibre
--- ctFIRE/CPP/makefile.m
+++ ctFIRE/CPP/makefile.m
@@ -1,2 +1,8 @@
-mex -v findlocmax_native.cpp CXXFLAGS="$CXXFLAGS -fopenmp" LDFLAGS="$LDFLAGS -fopenmp"
-mex -v extend_xlink_native.cpp CXXFLAGS="$CXXFLAGS -fopenmp" LDFLAGS="$LDFLAGS -fopenmp"
+% for macOS, user must have openmp installed. Installation instructions on https://mac.r-project.org/openmp/ 
+if ~ismac
+    mex -v findlocmax_native.cpp CXXFLAGS="$CXXFLAGS -fopenmp" LDFLAGS="$LDFLAGS -fopenmp"
+    mex -v extend_xlink_native.cpp CXXFLAGS="$CXXFLAGS -fopenmp" LDFLAGS="$LDFLAGS -fopenmp"
+else 
+    mex -v findlocmax_native.cpp CPPFLAGS="$CPPFLAGS -Xclang -fopenmp" LDFLAGS="$LDFLAGS -lomp"
+    mex -v extend_xlink_native.cpp CPPFLAGS="$CPPFLAGS -Xclang -fopenmp" LDFLAGS="$LDFLAGS -lomp"
+end 
