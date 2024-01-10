2407bc7fb483e24e944de53e25c53a894cd5e0c4
Yuming Liu <liu372@wisc.edu>
2024-01-10 16:20:11 -0600

    Try fixing the issue of unexpected termination of pyenv
    This is probably due to the lack of access to the conda environment or python packages

---
+++ Cellanalysis/loadCondaenvironment.m
@@ -0,0 +1,21 @@
+function loadCondaenvironment(pythonDir)
+
+%loadCondaenvironment.m
+%load cusconda environment
+pythonDir = 'C:\Users\liu372\.conda\envs\CApy311\python.exe';
+pyExec = pythonDir;
+pyRoot = fileparts(pyExec);
+p = getenv('PATH');
+p = strsplit(p, ';');
+addToPath = {
+    pyRoot
+    fullfile(pyRoot, 'Library', 'mingw-w64', 'bin')
+    fullfile(pyRoot, 'Library', 'usr', 'bin')
+    fullfile(pyRoot, 'Library', 'bin')
+    fullfile(pyRoot, 'Scripts')
+    fullfile(pyRoot, 'bin')
+    };
+p = [addToPath(:); p(:)];
+p = unique(p, 'stable');
+p = strjoin(p, ';');
+setenv('PATH', p);
