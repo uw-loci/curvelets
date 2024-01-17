d06564af1237eacdaf9713ad80b60a98b6f3c5a7
Yuming Liu <liu372@wisc.edu>
2024-01-17 16:30:36 -0600

    Add the main function for skeleton-based intersection points detection

---
+++ ctFIRE/FiberCenterlineExtraction/IPdetection.py
@@ -0,0 +1,35 @@
+# import pandas as pd
+# from glob import glob
+# import math
+# import numpy as np
+from skimage import io, morphology, img_as_ubyte, img_as_float, filters
+# import os
+# from tqdm.notebook import tqdm
+from matplotlib import pyplot as plt
+# from shapely.geometry import LineString, MultiPoint
+# from shapely.ops import split
+from scipy import io as sio
+from centerline import CenterLine, smooth_mask, iou
+def IP_skeleton(dataPath,imagePath,coordPath):
+    # dataPath = 'examples/sample_ctFIRE.mat'
+    # imagePath = "examples/sample_input.png"
+    # coordPath = 'IPxy_skeleton.mat'
+    mat = sio.loadmat(dataPath)
+    mat_data = mat['data']
+    centerline_mat = CenterLine()
+    line_dict = centerline_mat.mat_to_lines(mat_data)
+    centerline_mat = CenterLine(line_dict=line_dict, associate_image=io.imread(imagePath))
+    fig, ax = plt.subplots(1, 3, figsize=(20, 20))
+    ax[0].imshow(centerline_mat.associate_image, cmap=plt.cm.gray)
+    ax[1].imshow(centerline_mat.centerline_image, cmap=plt.cm.gray)
+    joints_coords, filtered_image = centerline_mat.joint_filter(centerline_mat.centerline_image)
+    ax[2].plot(joints_coords[:, 1], joints_coords[:, 0], color='cyan', marker='o',
+            linestyle='None', markersize=6)
+    ax[2].imshow(filtered_image, cmap=plt.cm.gray)
+    ax[2].plot(joints_coords[:, 1], joints_coords[:, 0], color='cyan', marker='o',
+            linestyle='None', markersize=6)
+    plt.show()
+    mdic = {"IPxy_skeleton": joints_coords, "label": "IP from python skeleton-based computation"}
+    sio.savemat(coordPath,mdic)
+    return joints_coords
+    # centerline_mat.export_line_dict('examples/example_image_ctFIRE_line_dict.csv')
---
+++ ctFIRE/skeletonIntersection.m
@@ -0,0 +1,56 @@
+function IPxy_skeleton = skeletonIntersection(dataPath,imagePath,coordPath)
+% This fuction uses skeleton to find the intersection point
+% based on a python module named centerline
+% input: 
+%   dataPath: full path to the CT-FIRE output .mat file, e.g.:
+%              dataPath ='H:\GitHub.06.2022\AnalysisTools\testImages\ctFIREout\ctFIREout_synSize512-fiber100.mat';
+%   imagePath: full path to the original image, e.g.:
+%               imagePath= 'H:\GitHub.06.2022\AnalysisTools\testImages\synSize512-fiber100.tif';
+%   coordPath: full path to the output .mat file containing the coordinates
+%   [Y X] of the IP points, e.g.:
+%               coordPath = fullfile(pwd,'IPxy_skeleton.mat');
+% output: 
+% 1) save IP coordinates into a .mat file with the full path of coordPath
+% 2) IPxy_skeleton: coordinates of the detected intersection points
+% Example:
+% dataPath ='H:\GitHub.06.2022\AnalysisTools\testImages\ctFIREout\ctFIREout_synSize512-fiber100.mat'
+% imagePath= 'H:\GitHub.06.2022\AnalysisTools\testImages\synSize512-fiber100.tif'
+% coordPath = fullfile(pwd,'IPxy_skeleton.mat')
+% skeletonIntersection(dataPath,imagePath,coordPath)
+addpath('./FiberCenterlineExtraction');
+pyenv
+terminate(pyenv)
+py.sys.path;
+ % 'C:\\Program Files\\MATLAB\\R2023b\\interprocess\\bin\\win64\\pycli',
+ % '', 'C:\\ProgramData\\miniconda3\\envs\\collagen\\python311.zip', 
+ % 'C:\\ProgramData\\miniconda3\\envs\\collagen\\DLLs', 
+ % 'C:\\ProgramData\\miniconda3\\envs\\collagen\\Lib', 
+ % 'C:\\ProgramData\\miniconda3\\envs\\collagen', 
+ % 'C:\\ProgramData\\miniconda3\\envs\\collagen\\Lib\\site-packages', 
+ % 'C:\\ProgramData\\miniconda3\\envs\\collagen\\Lib\\site-packages\\win32', 
+ % 'C:\\ProgramData\\miniconda3\\envs\\collagen\\Lib\\site-packages\\win32\\lib', 
+ % 'C:\\ProgramData\\miniconda3\\envs\\collagen\\Lib\\site-packages\\Pythonwin']
+insert(py.sys.path,int32(0),fullfile(pwd,'FiberCenterlineExtraction'))
+% clear classes
+IPskeleton = py.importlib.import_module('IPdetection');
+try
+    IPxy_skeleton = double(IPskeleton.IP_skeleton(dataPath,imagePath,coordPath));
+catch EXP1
+    fprintf("Skeleton-based IP detection didnot go through. Error mesage: %s \n",EXP1.message)
+    if exist("coordPath",'file')
+       load(coordPath,'IPxy_skeleton')
+       disp("Using the pre-saved IP coordinates output from directly running the python module" )
+    else
+       fprintf('No IP coordinates from skeleton-based analysis was found') 
+       IPxy_skeleton =[];
+       return
+    end
+
+end
+% %% visualize the IP points
+figure, imshow(imagePath)
+axis image equal
+colormap gray
+hold on
+plot(IPxy(:,2),IPxy(:,1),'ro','MarkerSize',5)
+hold off
