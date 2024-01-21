8b203367e5499172626435a8791f7f3d3c0850dd
Yuming Liu <liu372@wisc.edu>
2024-01-21 13:25:32 -0600

    Donot show individual fiber tracking during the analysis
    Enable the flag of plot in debugging mode only

--- ctFIRE/fire_2D_ang1.m
+++ ctFIRE/fire_2D_ang1.m
@@ -214,7 +214,7 @@ M = network_statK(Xas,Fa,Va,Ra);  % ym: modification made in this function
 
 %ym: interpolation of the fibers
 %p.s_maxspace = 3;
-[Xai Fai Vai] = fiber2beam(Xas,Fa,Va,Ra,p.s_maxspace,p.lambda,1);  % yl: add test plot in this function
+[Xai Fai Vai] = fiber2beam(Xas,Fa,Va,Ra,p.s_maxspace,p.lambda,0);  % yl: add test plot in this function
 data.Xai = Xai;
 data.Fai = Fai;
 data.Vai = Vai;
