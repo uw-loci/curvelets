22bb107105da86a3e40cc27b4e8b3343fdce9a5b
Aarush Umap <aarush.umap@gmail.com>
2024-03-18 15:02:18 -0500

    Init of cellpose predict function

---
+++ src/CurveAlign_CT-FIRE/Cellanalysis/toolevaluation/cellpose_segmentation.py
@@ -0,0 +1,16 @@
+from cellpose import io, models
+from pretrained_segmentation import IPretrained
+
+def predict(model_name, input_data_path, **kwargs):
+    model = None
+    if model_name != 'cyto' or model_name != 'cyto2' or model_name != 'cyto3' or model_name != 'nuclei':
+        model = models.CellposeModel(model_type=model_name)
+    else:
+        model = models.Cellpose(model_type=model_name)
+
+    image = io.imread(input_data_path)
+    if len(image.shape) != 3:
+        Exception(f"Image is of shape: {image.shape} when Cellpose is expecting (Y,X,C) or (C,Y,X)")
+
+    # if image.shape[0] > 2 and image.shape[2] > 2:
+    #     Exception(f"Image is of shape: {image.shape} when Cellpose is expecting (Y,X,C) or (C,Y,X)")
