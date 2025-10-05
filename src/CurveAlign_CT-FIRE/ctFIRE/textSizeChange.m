e48f8fbd6cc5ea2665318fcfbaef6ee89d9a47b0
Yuming Liu <liu372@wisc.edu>
2025-10-05 17:35:59 -0500

    Delete unnecessary m files

--- src/CurveAlign_CT-FIRE/ctFIRE/textSizeChange.m
+++
@@ -1,9 +0,0 @@
-% takes the screen size - finds the height of the screen divides by a ratio
-% of 768/8 and scales up the text by the same amount
-function[]=textSizeChange(figNumber)
-    fig=figNumber;
-    SS=get(0,'screensize');
-    ratio=768/8;%768 is based on a computer
-    newTextSize=floor(SS(4)/ratio);
-    set(findall(fig,'-property','FontSize'),'FontSize',newTextSize);
-end
